d57e63467bd69d6ad3e5eceaabb4a84b3afa9384
Raul_Canton <raulcantong@hotmail.com>
2024-04-13 20:13:00 +0200

    MAIN

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,22 +2,37 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 import org.iesalandalus.programacion.reservashotel.Controlador.Controlador;
+import org.iesalandalus.programacion.reservashotel.Modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.reservashotel.Modelo.IModelo;
 import org.iesalandalus.programacion.reservashotel.Modelo.Modelo;
+import org.iesalandalus.programacion.reservashotel.Modelo.negocio.IFuenteDatos;
 import org.iesalandalus.programacion.reservashotel.vista.Vista;
 
 public class MainApp {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
 
-            Modelo modelo = new Modelo();
+            IModelo modelo = new Modelo(procesarArgumentosFuenteDatos(args));
             Vista vista = new Vista();
             Controlador controlador = new Controlador(modelo, vista);
             controlador.comenzar();
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static IFuenteDatos procesarArgumentosFuenteDatos(String[] args) {
+        IFuenteDatos fuenteDatos = FactoriaFuenteDatos.MONGODB.crear();
+        for (String argumento : args) {
+            if (argumento.equalsIgnoreCase("-fdmongodb")) {
+                fuenteDatos = FactoriaFuenteDatos.MONGODB.crear();
+            } else if (argumento.equalsIgnoreCase("-fdmemoria")) {
+                fuenteDatos = FactoriaFuenteDatos.MEMORIA.crear();
+            }
+        }
+        return fuenteDatos;
     }
 }
